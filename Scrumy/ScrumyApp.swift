1a17438598bd1d28f3fe1f914e3b30f4feeb3405
MUmarJ <umarjh96@gmail.com>
2023-01-25 08:27:20 -0600

    Initial Commit

---
+++ Scrumy/ScrumyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumyApp.swift
+//  Scrumy
+//
+//  Created by MUmarJ on 1/25/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
