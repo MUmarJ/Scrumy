048a15924023929a1f19c78a673be001c5ae0b5b
MUmarJ <umarjh96@gmail.com>
2023-01-31 20:43:18 -0600

    Add DetailView for each Scrum + Add Navigation

---
+++ Scrumy/DetailView.swift
@@ -0,0 +1,56 @@
+//
+//  DetailView.swift
+//  Scrumy
+//
+//  Created by MUmarJ on 1/31/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        List {
+            Section(header: Text("Meeting Info")) {
+                NavigationLink(destination: MeetingView()) {
+                    Label("Start Meeting", systemImage: "timer")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+                }
+                HStack {
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                    .accessibilityElement(children: .combine)
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text("\(scrum.theme.name)")
+                        .padding(5)
+                        .background(scrum.theme.mainColor)
+                        .foregroundColor(scrum.theme.accentColor)
+                        .cornerRadius(5)
+
+                }
+                    .accessibilityElement(children: .combine)
+            }
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
+                    Label(attendee.name, systemImage: "person")
+
+                }
+            }
+        }
+            .navigationTitle(scrum.title)
+    }
+}
+
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            DetailView(scrum: DailyScrum.sampleData[0])
+        }
+    }
+}
